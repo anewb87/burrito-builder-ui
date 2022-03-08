f13ac0799667d3b844269456d8488d0e6ccf5c2e
Alexis Newby <alexis.newby@gmail.com>
2022-03-08 12:37:13 -0700

    After time, couldn't help it- fixed error message about keys on the ingredient li

--- src/components/OrderCard/OrderCard.js
+++ src/components/OrderCard/OrderCard.js
@@ -3,12 +3,15 @@ import './OrderCard.css'
 
 const OrderCard = ({ name, ingredients }) => {
 
+    let keyNum = 0
+
     return (
         <div data-testid='cards' className='card'>
             <h3>{name}</h3>
             <ul className='ingredient-list'>
                 {ingredients.map(ingredient => {
-                    return <li>{ingredient}</li>
+                    keyNum++
+                    return <li key={keyNum}>{ingredient}</li>
                 })}
             </ul>
         </div>
